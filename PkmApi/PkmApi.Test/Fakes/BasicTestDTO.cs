09a8b3807c0e39180f5bed99795178ac4ddb89ee
Joseph Park <josephpkc@gmail.com>
2025-04-03 18:57:54 -0700

    (fix): Some test fixes

--- PkmApi/PkmApi.Test/Fakes/BasicTestDTO.cs
+++ PkmApi/PkmApi.Test/Fakes/BasicTestDTO.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Immutable;
 using System.Text.Json.Serialization;
+
 using PkmApi.DTOs;
 
 namespace PkmApi.Test.Fakes
 {
     public record BasicTestDTO(
+        [property: JsonPropertyName("name")]
         string                          Name,
+        [property: JsonPropertyName("id")]
         int                             Id,
         [property: JsonPropertyName("sub-data")]
         IImmutableList<BasicTestSDTO>   SubData
-    ) : BasePkmDTO(Id, Name);
+    ) : IPkmApiDTO;
 
     public record BasicTestSDTO(
         [property: JsonPropertyName("value")]
