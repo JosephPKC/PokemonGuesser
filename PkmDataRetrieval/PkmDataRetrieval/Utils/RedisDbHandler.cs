8c7e6493b5c66f3c521ca51a8c68d8779bc345b6
Joseph Park <josephpkc@gmail.com>
2025-04-04 20:34:42 -0700

    (feat): Create a basic redis db handler and add swagger

---
+++ PkmDataRetrieval/PkmDataRetrieval/Utils/RedisDbHandler.cs
@@ -0,0 +1,53 @@
+﻿using NRedisStack.RedisStackCommands;
+using StackExchange.Redis;
+
+namespace PkmDataRetrieval.Utils
+{
+    //  A basic handler that makes common usage easier.
+    public class RedisDbHandler(IConnectionMultiplexer pRedisMutex, string pPrefixPath)
+    {
+        private readonly IDatabase _db = pRedisMutex.GetDatabase();
+        private readonly string _prefixPath = pPrefixPath;
+
+        public bool Add<TData>(string pKey, TData pData, int? pLifeInSec = null, bool pOverwrite = false) where TData : class
+        {
+            if (string.IsNullOrWhiteSpace(pKey))
+            {
+                return false;
+            }
+
+            string key = $"{_prefixPath}:{pKey}";
+
+            TData? item = Get<TData>(key);
+            if (!EqualityComparer<TData>.Default.Equals(item, default) && !pOverwrite)
+            {
+                return false;
+            }
+
+            bool result = _db.JSON().Set(key, "$", pData);
+            if (!result)
+            {
+                return false;
+            }
+
+            if (pLifeInSec == null)
+            {
+                return result;
+            }
+
+            return _db.KeyExpire(pKey, TimeSpan.FromSeconds(pLifeInSec.Value));
+        }
+
+        public TData? Get<TData>(string pKey) where TData : class
+        {
+            if (string.IsNullOrWhiteSpace(pKey))
+            {
+                return default;
+            }
+
+            string key = $"{_prefixPath}:{pKey}";
+
+            return _db.JSON().Get<TData>(key);
+        }
+    }
+}
