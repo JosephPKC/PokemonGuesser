ac493a9a8c991441ed2a3237a12af6d9a9ff0320
Joseph Park <josephpkc@gmail.com>
2025-04-07 16:17:34 -0700

    (feat): Small adjustments, primarily to trigger nuget push workflow

--- PkmApi/PkmApi/PkmApiManager.cs
+++ PkmApi/PkmApi/PkmApiManager.cs
@@ -4,17 +4,19 @@ using PkmApi.Utils;
 
 namespace PkmApi
 {
-    internal class PkmApiManager : IPkmApi
+    internal class PkmApiManager(string pVersion, 
+        IApiGetter? pApiGetter = null, IJsonParser? pJsonParser = null, ILogger? pLogger = null, 
+        ICacheFactory? pCacheFactory = null, int? pCacheSizeLimit = null, int? pCacheLifeInSec = null) 
+        : IPkmApi
     {
         private static readonly string _baseUri = "pokeapi.co/api";
 
-        public PkmApiManager(string pVersion, IApiGetter? pApiGetter = null, IJsonParser? pJsonParser = null, ILogger? pLogger = null, ICacheFactory? pCacheFactory = null, int? pCacheSizeLimit = null, int? pCacheLifeInSec = null)
-        {
-            Pokemon = EndpointHandlerFactory.BuildEndpointHandler<PkmDTO>(_baseUri, pVersion, "pokemon", pApiGetter ?? new HttpGetter(), pJsonParser ?? new JsonParser(), pLogger ?? new ConsoleLogger(), pCacheFactory, pCacheSizeLimit, pCacheLifeInSec);
-        }
-
         #region IPkmApi
-        public IEndpointHandler<PkmDTO> Pokemon { get; init; }
+        public IEndpointHandler<PkmDTO> Pokemon { get; init; } = EndpointHandlerFactory.BuildEndpointHandler<PkmDTO>(
+            _baseUri, pVersion, "pokemon", 
+            pApiGetter ?? new HttpGetter(), pJsonParser ?? new JsonParser(), pLogger ?? new ConsoleLogger(), 
+            pCacheFactory, pCacheSizeLimit, pCacheLifeInSec
+        );
         #endregion
     }
 }
