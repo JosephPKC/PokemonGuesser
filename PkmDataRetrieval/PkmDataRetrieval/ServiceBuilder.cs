89154256b6cf5c9fc74a26629633f362faa2e763
Joseph Park <josephpkc@gmail.com>
2025-04-22 21:43:08 -0700

    (feat): Some major docker/kube changes
    - Moved the dockerfiles one level down.
    - Added pkm web as a front end angular service. It will be deployed as an nginx container.
    - Split the kubernetes manifests to the appropriate services.
    - Adjusted the data retrieval redis connection to use the internal service name. It will be resolved via kubernetes dns.
    - Added some bash scripts to automate updating the kubernetes deployments when the manifests are updated, and one to automatically build docker images and push to the local kubernetes cluster for local dev.

--- PkmDataRetrieval/PkmDataRetrieval/ServiceBuilder.cs
+++ PkmDataRetrieval/PkmDataRetrieval/ServiceBuilder.cs
@@ -31,7 +31,7 @@ namespace PkmDataRetrieval
             pBuilder.Services.AddSwaggerGen();
             pBuilder.Services.AddOpenApi();
             //  DI
-            pBuilder.Services.AddSingleton<IDataRetrieval>(DataRetrievalFactory.CreateDataRetriever(PkmGatewayFactory.CreateGateway(), ConnectionMultiplexer.Connect(Config.RedisConnect), Config.CurrentGenId));
+            pBuilder.Services.AddSingleton<IDataRetrieval>(DataRetrievalFactory.CreateDataRetriever(PkmGatewayFactory.CreateGateway(), ConnectionMultiplexer.Connect(Config.RedisKubeConnect, config => config.AbortOnConnectFail = false), Config.CurrentGenId));
         }
 
         private static void ConfigureForDevEnv(WebApplication pApp)
