582cf5c9522d7bfdee8c3db3456f6368e86276f2
Joseph Park <josephpkc@gmail.com>
2025-05-06 16:01:17 -0700

    (feat): Add basic structure and logic for game guess and hint processing.

---
+++ PkmGuessGame/PkmGuessGame/GuessGameModels.cs
@@ -0,0 +1,65 @@
+﻿namespace PkmGuessGame
+{
+    public class ProcessGuessResult
+    {
+        public GuessResults Result { get; set; }
+        public int Score { get; set; }
+        public int? GuessId { get; set; }
+        public bool IsGameDone { get; set; }
+        public int CurrentTotalScore { get; set; }
+    }
+
+    public enum GuessResults
+    {
+        Correct,
+        OldMatch,
+        Incorrect,
+        Duplicate
+    }
+
+    public class RevealHintResult
+    {
+        public HintResults Result { get; set; }
+        public string? Hint { get; set; }
+    }
+
+    public enum HintResults
+    {
+        Revealed,
+        AlreadyRevealed,
+        Missing,
+        AlreadyAnswered
+    }
+
+    public class GameStats
+    {
+        public int NbrOfGuessesTotal { get; set; }
+        public int NbrOfCorrectGuesses { get; set; }
+        public int NbrOfOldCorrectGuesses { get; set; }
+        public int NbrOfIncorrectGuesses { get; set; }
+        public int TotalScore { get; set; }
+    }
+
+    public class PkmGameModel
+    {
+        public Dictionary<int, PkmMoveModel> Moves { get; set; } = [];
+        public Dictionary<int, PkmOldMoveModel> OldMoves { get; set; } = [];
+    }
+
+    public class PkmMoveModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        /* Hints */
+        public string Type { get; set; } = string.Empty;
+        public string MoveDamageClass { get; set; } = string.Empty;
+        public string FlavorText { get; set; } = string.Empty;
+    }
+
+    public class PkmOldMoveModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ PkmGuessGame/PkmGuessGame/ServiceBuilder.cs
@@ -0,0 +1,43 @@
+﻿namespace PkmGuessGame
+{
+    internal static class ServiceBuilder
+    {
+        public static WebApplication BuildServiceApp(string[] pArgs)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(pArgs);
+            ConfigureServices(builder);
+
+            WebApplication app = builder.Build();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            ConfigureForDevEnv(app);
+
+            return app;
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder pBuilder)
+        {
+            //  Core Services
+            pBuilder.Services.AddControllers();
+            pBuilder.Services.AddEndpointsApiExplorer();
+            pBuilder.Services.AddSwaggerGen();
+            pBuilder.Services.AddOpenApi();
+
+            //  DI
+        }
+
+        private static void ConfigureForDevEnv(WebApplication pApp)
+        {
+            if (!pApp.Environment.IsDevelopment())
+            {
+                return;
+            }
+
+            pApp.MapOpenApi();
+            pApp.UseSwagger();
+            pApp.UseSwaggerUI();
+        }
+    }
+}
